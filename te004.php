11b1b077fb58facd0205bfb2fba4406e2306681b
Luís Mendes <luis.mendes070@outlook.com.br>
2016-12-02 21:24:42 -0200

    tela004

---
+++ te004.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "PHP!";
+
+$opcao = $_POST['descricao'];
+
+echo "opcao = ";
+echo $opcao;
+
+switch($opcao){
+	
+	case 1:
+		break;
+		
+	case 2:
+		break;
+		
+	case 3:
+		break;
+		
+	default:;
+	
+}
+
+?>
